35b79005daaa6a4639758d7167ff8766c132110c
dd4897 <1271446998@qq.com>
2024-03-07 16:12:32 +0800

    UpdateObjectID

--- app/api/user.py
+++ app/api/user.py
@@ -3,7 +3,6 @@ from app.schemas.user import User
 from typing import List
 from app.config.db import user_collection
 from bson import ObjectId
-import time,uuid
 router = APIRouter()
 
 
@@ -15,15 +14,12 @@ async def create_user(user: User) -> User:
     created_user = await user_collection.find_one({"_id": user_obj.inserted_id})
     if created_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    # 将 ObjectId 转换成字符串
     created_user['_id'] = str(created_user['_id'])
-    # 这里我们直接返回 Pydantic 模型，它会处理 _id 到 id 的转换及序列化
     return User(**created_user)
 # 获取所有用户
 @router.get("/users/", response_description="List all users", response_model=List[User])
 async def list_users():
     users = await user_collection.find().to_list(1000)
-    # 手动转换 _id 从 ObjectId 到字符串
     for user in users:
         user["_id"] = str(user["_id"])
     return users
@@ -35,13 +31,11 @@ async def find_user(id: str):
         return user
     raise HTTPException(status_code=404, detail=f"User {id} not found")
 
-# 更新用户
 @router.put("/users/{id}", response_description="Update a user", response_model=User)
 async def update_user(id: str, user: User):
     user = {k: v for k, v in user.dict().items() if v is not None}
     if len(user) >= 1:
         update_result = await user_collection.update_one({"_id": ObjectId(id)}, {"$set": user})
-
         if update_result.modified_count == 1:
             if (
                 updated_user := await user_collection.find_one({"_id": ObjectId(id)})
