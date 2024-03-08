3266d3c18ded3b4c90b4b25b79a90737492010fe
dd4897 <1271446998@qq.com>
2024-03-08 14:29:16 +0800

    FixChatMemory

--- app/api/conversation.py
+++ app/api/conversation.py
@@ -1,17 +1,10 @@
-from fastapi import APIRouter, HTTPException,Body
-from app.schemas.conversation import Conversation,ChatInput
+from fastapi import APIRouter, HTTPException, Body
+from app.schemas.conversation import Conversation, ChatInput
 from app.config.db import conversation_collection
-import os
-from uuid import uuid4
 from datetime import datetime
-from openai import OpenAI
-from dotenv import load_dotenv, find_dotenv
+from app.utils.tools import get_uuid, openai as client
+
 router = APIRouter()
-_ = load_dotenv(find_dotenv())
-openai = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY"),
-    base_url=os.getenv("OPENAI_BASE_URL")
-)
 
 
 @router.post("/chat", response_model=Conversation, response_description="Chat with GPT")
@@ -23,14 +16,16 @@ async def chat_completion(chat_input: ChatInput = Body(...)):
     # 如果存在以前的对话，将其逆序加入到messages中，因为我们要最早的消息在前
     messages = []
     if previous_conversations:
+        # print(previous_conversations)
         for conv in reversed(previous_conversations):
+            messages.append({"role": "user", "content": conv['prompt']})
             messages.append({"role": conv["role"], "content": conv["content"]})
-
     # 添加当前用户的提示
     messages.append({"role": "user", "content": chat_input.prompt})
+    print(messages)
     try:
         # 使用openai库调用ChatGPT
-        response = openai.chat.completions.create(
+        response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=messages,
             temperature=0
@@ -40,20 +35,20 @@ async def chat_completion(chat_input: ChatInput = Body(...)):
     # 解析ChatGPT的响应
     chatgpt_response = response.choices[0].message.content
     # 创建对话记录
+    messages.append({'role': "assistant", "content": chatgpt_response})
     conversation = Conversation(
-        session_id=str(uuid4()),  # 此处生成session_id，您可以根据需要进行更改
+        session_id=get_uuid(),  # 此处生成session_id，您可以根据需要进行更改
         user_id=chat_input.user_id,
         model="gpt-3.5-turbo",
-        answer_id=str(uuid4()),
-        parent_id=None,  # 如果此消息是对另一个消息的回复，则设置相应的parent_id
+        answer_id=get_uuid(),
         prompt=chat_input.prompt,
         role="assistant",  # 假设AI的角色为'assistant'
         content=chatgpt_response,
         update_time=datetime.now(),
-        create_time=datetime.now()
+        create_time=datetime.now(),
+        messages=messages
     )
     # 将对话保存到MongoDB
     await conversation_collection.insert_one(conversation.dict())
     # 返回Conversation模型作为响应体
     return conversation
-
--- app/utils/tools.py
+++ app/utils/tools.py
@@ -1,3 +1,12 @@
-import time
 import uuid
+from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
+import os
+def get_uuid():
+    return uuid.uuid4()
 
+_ = load_dotenv(find_dotenv())
+openai = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url=os.getenv("OPENAI_BASE_URL")
+)
